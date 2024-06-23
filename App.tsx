1d3d50ec3aceec58a38ec437c63e7a7448e0241b
José Ricardo <josericardosaraiva7@gmail.com>
2024-06-23 11:00:13 -0300

    config: One Signal

--- App.tsx
+++ App.tsx
@@ -2,12 +2,16 @@ import { StatusBar } from 'react-native';
 import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
+import { OneSignal } from 'react-native-onesignal';
+
 import { Routes } from './src/routes';
 
 import { THEME } from './src/theme';
 import { Loading } from './src/components/Loading';
 
 import { CartContextProvider } from './src/contexts/CartContext';
+
+OneSignal.initialize("0735e945-ded8-4a5c-ad1b-0b959cd5d6ac");
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
