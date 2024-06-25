594e8d0c9dc4a0673af56dbf1a6468706bbd29cc
José Ricardo <josericardosaraiva7@gmail.com>
2024-06-25 16:59:12 -0300

    feat: display foreground notification

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,17 +1,46 @@
+import { useEffect, useState } from 'react';
 import { useTheme } from 'native-base';
 import { DefaultTheme, NavigationContainer } from '@react-navigation/native';
+import { OneSignal, NotificationWillDisplayEvent, OSNotification } from 'react-native-onesignal';
 
 import { AppRoutes } from './app.routes';
+import { Notification } from '../components/Notification';
+
 
 export function Routes() {
   const { colors } = useTheme();
+  const [notification, setNotification] = useState<OSNotification>();
 
   const theme = DefaultTheme;
   theme.colors.background = colors.gray[700];
 
+  useEffect(() => {
+    const handleNotification = (event: NotificationWillDisplayEvent): void => {
+      event.preventDefault()
+      const response = event.getNotification()
+      setNotification(response)
+    }
+
+    OneSignal.Notifications.addEventListener(
+      "foregroundWillDisplay", 
+      handleNotification
+    )
+
+    //remove from memory when component is unmounted
+    return () => OneSignal.Notifications.removeEventListener(
+      "foregroundWillDisplay", 
+      handleNotification
+    )
+  },[])
+
   return (
     <NavigationContainer theme={theme}>
       <AppRoutes />
+
+      {
+        notification?.title && (
+        <Notification title={notification.title} onClose={() => setNotification(undefined)}/>
+      )}
     </NavigationContainer>
   );
 }
