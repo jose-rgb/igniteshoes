92220aa9403bdbe9e30e7e61d9fe29309e95a033
José Ricardo <josericardosaraiva7@gmail.com>
2024-06-26 12:26:33 -0300

    checking click on button action

--- App.tsx
+++ App.tsx
@@ -23,7 +23,15 @@ export default function App() {
 
   useEffect(() => {
     const handleNotificationClick = (event: NotificationClickEvent): void => {
-      console.log("Notificação Aberta")
+      const { actionId } = event.result
+
+      if(actionId === "1") {
+        return console.log("Ver todos")
+      }
+
+      if(actionId === "2") {
+        return console.log("Ver pedido")
+      }
     }
 
     OneSignal.Notifications.addEventListener("click", handleNotificationClick)
