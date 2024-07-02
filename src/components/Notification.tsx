6d54139a0961175b34e9cf130cc30b26af514a5f
José Ricardo <josericardosaraiva7@gmail.com>
2024-07-02 13:57:00 -0300

    feat: Deep Linking with App in the foreground

--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -1,27 +1,19 @@
 import { HStack, Text, IconButton, CloseIcon, Icon, Pressable } from 'native-base';
 import { Ionicons } from '@expo/vector-icons';
 import { OSNotification } from 'react-native-onesignal';
-import { useNavigation } from '@react-navigation/native';
+import * as Linking from 'expo-linking';;
 
 type Props = {
   data: OSNotification;
   onClose: () => void;
 }
 
-type AdditionalDataProps = {
-  route?: string
-  product_id?: string
-}
-
 export function Notification({ data, onClose }: Props) {
-  const { navigate } = useNavigation();
-
+  
   function handleOnPres() {
-    const { route, product_id } = data.additionalData as AdditionalDataProps;
-
-    if(route === "details" && product_id) {
-      navigate("details", {productId: product_id})
-      onClose()
+    if(data.launchURL) {
+      Linking.openURL(data.launchURL);
+      onClose();
     }
   }
 
