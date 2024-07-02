1b5fbcb107436ebc463d4545c799ff7671b33133
José Ricardo <josericardosaraiva7@gmail.com>
2024-07-02 11:31:38 -0300

    feat: Setting deep linking

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,6 +6,19 @@ import { OneSignal, NotificationWillDisplayEvent, OSNotification } from 'react-n
 import { AppRoutes } from './app.routes';
 import { Notification } from '../components/Notification';
 
+const linking = {
+  prefixes: ["igniteshoes://", "com.drjsoftwares.igniteshoes://"],
+  config: {
+    screens: {
+      details: {
+        path: "/details/:productId",
+        parse: {
+          productId: (productId: string) => productId,
+        },
+      },
+    },
+  },
+}
 
 export function Routes() {
   const { colors } = useTheme();
@@ -34,7 +47,7 @@ export function Routes() {
   },[])
 
   return (
-    <NavigationContainer theme={theme}>
+    <NavigationContainer theme={theme} linking={linking}>
       <AppRoutes />
 
       {
